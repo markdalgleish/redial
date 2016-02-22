01671bf6171716d7d9e8670576e9aad15c87c225
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2016-02-22 18:19:18 +1100

    Simplify trigger filter step

--- src/trigger.js
+++ src/trigger.js
@@ -1,8 +1,8 @@
 export default (name, components, locals) => {
   const promises = (Array.isArray(components) ? components : [components])
 
-    // Filter out routes without defined components
-    .filter(component => !!component)
+    // Filter out falsy components
+    .filter(component => component)
 
     // Get component lifecycle handler functions
     .map(component => ({ component, handlers: component.__redial_handlers__ }))
